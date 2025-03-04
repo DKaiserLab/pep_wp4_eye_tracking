4ef849d5ee67f0585a3fa4643210d26273e7ba66
michaengeser <125463198+michaengeser@users.noreply.github.com>
2025-03-04 13:26:50 +0100

    update analysis

---
+++ code/functions/doPermutations.m
@@ -0,0 +1,74 @@
+function res = doPermutations(matin2D, rval, cfg)
+
+% get default values
+if ~isfield(cfg, 'permutation_type'); cfg.permutation_type = 'row_col_shuffle_ref';end
+if ~isfield(cfg, 'permutation_test'); cfg.permutation_test = false;end
+if ~isfield(cfg, 'n_permutations'); cfg.n_permutations = 10000;end
+if ~isfield(cfg, 'partial_cor'); cfg.partial_cor = false;end
+if ~isfield(cfg, 'random_seqs')
+    % generate permutated subjects list
+    cfg.random_seqs = cell(width(matin2D), cfg.n_permutations);
+    for i = 1:cfg.n_permutations
+        if ismember(cfg.permutation_type, {'row_col_shuffle_ref',...
+                'row_col_shuffle_pred', 'row_col_shuffle_pred_all', 'row_col_shuffle_pred_plus_reoder'})
+            % get random sequence of rows and columns
+            for ii = 1:width(matin2D)
+                cfg.random_seqs{ii,i} = randperm(cfg.n);
+            end
+        elseif strcmp(cfg.permutation_type, 'sign_flip_ref')
+            % get random rows and columns to flip sign
+            cfg.random_seqs{1,i} = randperm(cfg.n, randi([1, cfg.n]));
+        end
+    end
+end
+
+% make random permutations
+for iRDM = 1:width(matin2D)
+    permutation_RDMs(iRDM).RDM = squareform(matin2D(:, iRDM));
+    permutation_RDMs(iRDM).name = ['RDM', num2str(iRDM)];
+end
+orginal_RDMs = permutation_RDMs;
+res.perm_r_mat = zeros(width(matin2D)-1, cfg.n_permutations);
+
+for perm = 1:cfg.n_permutations
+    % randomize RDM
+    if strcmp(cfg.permutation_type, 'row_col_shuffle_ref')
+        % shuffle the order of rows and columns
+        ref_RDM = orginal_RDMs(1);
+        ref_RDM.RDM = ref_RDM.RDM(cfg.random_seqs{1,perm}, cfg.random_seqs{1,perm});
+        % replace reference RDM by permutated RDM in RDMs struct
+        permutation_RDMs(1) = ref_RDM;
+    elseif strcmp(cfg.permutation_type, 'sign_flip_ref')
+        ref_RDM_rand = ref_RDM;
+        % flip sign of row
+        ref_RDM_rand.RDM(cfg.random_seqs{1,perm},:) = -(ref_RDM.RDM(cfg.random_seqs{1,perm},:));
+        % flip sign of columns
+        ref_RDM_rand.RDM(:,cfg.random_seqs{1,perm}) = -(ref_RDM.RDM(:,cfg.random_seqs{1,perm}));
+        % replace reference RDM by permutated RDM in RDMs struct
+        permutation_RDMs(1) = ref_RDM_rand;
+    end
+
+    % run  correlation
+    if cfg.partial_cor
+        [~, r_mat, ~, ~] = partial_cor_RDM(cfg, permutation_RDMs);
+    else
+        [~, r_mat, ~] = cor_RDM(permutation_RDMs, cfg);
+    end
+    res.perm_r_mat(1:end, perm) = r_mat(2:end, 1);
+
+
+    if mod(perm/cfg.n_permutations, 0.1) == 0
+        disp([num2str((perm/cfg.n_permutations)*100), '% of permutations of is done'])
+    end
+end
+% get confidence interval and p value
+% get p value from random permutation (one-sided test aginst
+% permutation distribution)
+for row = height(res.perm_r_mat)
+    res.p_value(row) = sum(res.perm_r_mat(row,:) >= rval(row)) / cfg.n_permutations;
+
+    % get p values for categories
+    res.ci_upper(row) = rval - prctile(res.perm_r_mat(row,:), 5);
+    res.ci_lower(row) = rval - prctile(res.perm_r_mat(row,:), 95);
+end
+end
---
+++ code/functions/getSpliHalfReliability.m
@@ -0,0 +1,143 @@
+function d = getSpliHalfReliability(d, cfg)
+
+% loop through categories
+for iCate = 1:length(cfg.categories)
+    category = cfg.categories{iCate};
+
+     %% fixation count
+    [ObserverFixOdd, ~] = corr(d.GDM.(category).ObjectMultiFixated(:,d.GDM.(category).isOdd(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+    [ObserverFixEven, ~] = corr(d.GDM.(category).ObjectMultiFixated(:,~d.GDM.(category).isOdd(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+
+    % odd
+    ObserverFixOdd(logical(eye(size(ObserverFixOdd)))) = 0;
+    [A] = squareform(ObserverFixOdd);
+    % even
+    ObserverFixEven(logical(eye(size(ObserverFixEven)))) = 0;
+    [B] = squareform(ObserverFixEven);
+
+    % print correlation
+    disp(' ')
+    disp('Fixation count')
+    [R, p] = corr(A', B', 'Type', cfg.correlation_type);
+    disp(['r: ' num2str(R) ', p = ' num2str(p)]);
+    d.GDM.(category).splitHalfReliability.fixCount.r = R;
+
+    % do permutation test
+    res = doPermutations([A',B'], R, cfg);
+    d.GDM.(category).splitHalfReliability.fixCount.p = res.p_value;
+    d.GDM.(category).splitHalfReliability.fixCount.ci = [res.ci_lower, res.ci_upper];
+
+    %% single object dwell time
+    [ObserverMatOdd, ~] = corr(d.GDM.(category).ObjectDwellsMulti(:,d.GDM.(category).isOdd(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+    [ObserverMatEven, ~] = corr(d.GDM.(category).ObjectDwellsMulti(:,~d.GDM.(category).isOdd(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+
+    % runtime control
+    disp(' ')
+    disp(['Split-half reliablity for ', category, ' images'])
+
+    % odd
+    ObserverMatOdd(logical(eye(size(ObserverMatOdd)))) = 0;
+    [C] = squareform(ObserverMatOdd);
+    % even
+    ObserverMatEven(logical(eye(size(ObserverMatEven)))) = 0;
+    [D] = squareform(ObserverMatEven);
+
+    % print and store correlation
+    disp(' ')
+    disp('Single object dwell time')
+    [R, p] = corr(C', D','Type', cfg.correlation_type);
+    disp(['r: ' num2str(R) ', p = ' num2str(p)]);
+    d.GDM.(category).splitHalfReliability.singleObjects.r = R;
+
+    % do permutation test 
+    res = doPermutations([C',D'], R, cfg);
+    d.GDM.(category).splitHalfReliability.singleObjects.p = res.p_value;
+    d.GDM.(category).splitHalfReliability.singleObjects.ci = [res.ci_lower, res.ci_upper];
+
+   
+    %% object category dwell time
+    [ObserverMatOddCate, ~] = corr(d.GDM.(category).ObjectDwellsMultiCateOdd',...
+        'type', 'spearman', 'rows', 'complete');
+    [ObserverMatEvenCate, ~] = corr(d.GDM.(category).ObjectDwellsMultiCateEven',...
+        'type', 'spearman', 'rows', 'complete');
+
+    % odd
+    ObserverMatOddCate(logical(eye(size(ObserverMatOddCate)))) = 0;
+    [E] = squareform(ObserverMatOddCate);
+    % even
+    ObserverMatEvenCate(logical(eye(size(ObserverMatEvenCate)))) = 0;
+    [F] = squareform(ObserverMatEvenCate);
+
+    % print correlation
+    disp(' ')
+    disp('Category dwell time')
+    [R, p] = corr(E', F', 'Type', cfg.correlation_type);
+    disp(['r: ' num2str(R) ', p = ' num2str(p)]);
+    d.GDM.(category).splitHalfReliability.objectCate.r = R;
+
+    % do permutation test
+    res = doPermutations([E',F'], R, cfg);
+    d.GDM.(category).splitHalfReliability.objectCate.p = res.p_value;
+    d.GDM.(category).splitHalfReliability.objectCate.ci = [res.ci_lower, res.ci_upper];
+
+    %% first fix - single object dwell time
+
+    % runtime control
+    disp(' ')
+    disp(['Split-half reliablity for ', category, ' images on first fixation'])
+
+    [ObserverMatOdd, ~] = corr(d.GDM.(category).Objects_firstFix(:,d.GDM.(category).isOdd_firstFix(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+    [ObserverMatEven, ~] = corr(d.GDM.(category).Objects_firstFix(:,~d.GDM.(category).isOdd_firstFix(1,:))',...
+        'type', 'spearman', 'rows', 'complete');
+
+    % odd
+    ObserverMatOdd(logical(eye(size(ObserverMatOdd)))) = 0;
+    [G] = squareform(ObserverMatOdd);
+    % even
+    ObserverMatEven(logical(eye(size(ObserverMatEven)))) = 0;
+    [H] = squareform(ObserverMatEven);
+
+    % print and store correlation
+    disp(' ')
+    disp('Single object first fixation')
+    [R, p] = corr(G', H', 'Type', cfg.correlation_type);
+    disp(['r: ' num2str(R) ', p = ' num2str(p)]);
+    d.GDM.(category).splitHalfReliability.singleObjectsFirstFix.r = R;
+
+    % do permutation test
+    res = doPermutations([G',H'], R, cfg);
+    d.GDM.(category).splitHalfReliability.singleObjectsFirstFix.p = res.p_value;
+    d.GDM.(category).splitHalfReliability.singleObjectsFirstFix.ci = [res.ci_lower, res.ci_upper];
+
+    %% first fix - object category dwell time
+    [ObserverMatOddCate, ~] = corr(d.GDM.(category).ObjectsCate_firstFixOdd',...
+        'type', 'spearman', 'rows', 'complete');
+    [ObserverMatEvenCate, ~] = corr(d.GDM.(category).ObjectsCate_firstFixEven',...
+        'type', 'spearman', 'rows', 'complete');
+
+    % odd
+    ObserverMatOddCate(logical(eye(size(ObserverMatOddCate)))) = 0;
+    [I] = squareform(ObserverMatOddCate);
+    % even
+    ObserverMatEvenCate(logical(eye(size(ObserverMatEvenCate)))) = 0;
+    [J] = squareform(ObserverMatEvenCate);
+
+    % print correlation
+    disp(' ')
+    disp('Category first fixation count')
+    [R, p] = corr(I', J', 'Type', cfg.correlation_type);
+    disp(['r: ' num2str(R) ', p = ' num2str(p)]);
+    d.GDM.(category).splitHalfReliability.objectCateFirstFix.r = R;
+
+    % do permutation test
+    res = doPermutations([I',J'], R, cfg);
+    d.GDM.(category).splitHalfReliability.objectCateFirstFix.p = res.p_value;
+    d.GDM.(category).splitHalfReliability.objectCateFirstFix.ci = [res.ci_lower, res.ci_upper];
+end
+end
+
