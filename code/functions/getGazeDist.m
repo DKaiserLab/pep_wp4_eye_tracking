5ca3482b8758aca98b9cb61d39d4ef974d8bc555
michaengeser <125463198+michaengeser@users.noreply.github.com>
2025-06-10 15:30:46 +0200

    add bined analysis for gaze distance

--- code/functions/getGazeDist.m
+++ code/functions/getGazeDist.m
@@ -1,5 +1,9 @@
 function d = getGazeDist(d, cfg)
 
+if ~isfield(cfg, 'stimDur'); cfg.stimDur = 3; end
+if ~isfield(cfg, 'ETSamplingFrequency'); cfg.ETSamplingFrequency = 120; end % in Hz
+if ~isfield(cfg, 'binSize'); cfg.binSize = 0.05; end % in sec
+
 % Define directories and files
 dataDir = fullfile(pwd, '..', 'derivatives');
 files = dir(fullfile(dataDir, 'sub-*_task-EyeTracking_physio_R*.txt'));
@@ -23,8 +27,11 @@ bathImgs = log_file.trial(strcmp(log_file.category, 'bathroom'))';
 kitImgs = log_file.trial(strcmp(log_file.category, 'kitchen'))';
 
 % Initialize results
-bath_dists = nan(length(bathImgs), cfg.n, cfg.n);
-kit_dists = nan(length(kitImgs), cfg.n, cfg.n);
+nTimesteps = cfg.stimDur*cfg.ETSamplingFrequency;
+bathDists = nan(length(bathImgs), cfg.n, cfg.n);
+d.bathDistsAll = nan(length(kitImgs), cfg.n, cfg.n, nTimesteps);
+kitDists = bathDists;
+d.kitDistsAll = bathDists;
 
 for i = 1:height(log_file)
     
@@ -33,7 +40,6 @@ for i = 1:height(log_file)
 
     % Load gaze data from all subjects for this image
     gazeData = cell(1, cfg.n);
-    allData = cell(1, cfg.n);
     for s = 1:cfg.n
         dataFilePath = fullfile(dataDir, sprintf('sub-%03d', cfg.subNums(s)), 'samples',...
             sprintf('sub-%03d_task-EyeTracking_physio_R%03d.txt', cfg.subNums(s), i));
@@ -52,7 +58,6 @@ for i = 1:height(log_file)
         y = mean([data.gaze_point_LY, data.gaze_point_RY], 2, 'omitnan');
 
         gazeData{s} = [x y];
-        allData{s} = data;
     end
 
     % Make all trajectories the same length
@@ -67,6 +72,7 @@ for i = 1:height(log_file)
     % Pairwise Euclidean distances
     nSubs = cfg.n;
     pairDists = zeros(cfg.n, cfg.n);
+    pairDistsAll = zeros(cfg.n, cfg.n, size(gazeMatrix, 1));
     if isempty(gcp('nocreate'))
         parpool(8);
     end
@@ -79,6 +85,7 @@ for i = 1:height(log_file)
             trajB = squeeze(gazeMatrix(:,:,b));
             dist = sqrt(sum((trajA - trajB).^2, 2));
             pairDists(a,b) = median(dist, 'omitnan');
+            pairDistsAll(a,b,:) = dist;
         end
     end
     pairDists = squareform(squareform(pairDists));
@@ -86,25 +93,64 @@ for i = 1:height(log_file)
     % Store by category
     if ismember(i, bathImgs)
         idx = find(i == bathImgs);
-        bath_dists(idx, :, :) = pairDists;
+        bathDists(idx, :, :) = pairDists;
+        d.bathDistsAll(idx, :, :, 1:length(pairDistsAll)) = pairDistsAll;
     elseif ismember(i, kitImgs)
         idx = find(i == kitImgs);
-        kit_dists(idx, :, :) = pairDists;
+        kitDists(idx, :, :) = pairDists;
+        d.kitDistsAll(idx, :, :, 1:length(pairDistsAll)) = pairDistsAll;
     end
 end
 
-% Final results: mean across images per category
-d.kitchen_RDM.ratingRDM(end+1).RDM = squeeze(mean(kit_dists, 'omitnan'))/...
-    max(kit_dists,[], 'all');
-d.kitchen_RDM.ratingRDM(end).name = 'GazeDist';
-d.kitchen_RDM.ratingRDM(end).color = [0, 0, 0]';
-d.bathroom_RDM.ratingRDM(end+1).RDM = squeeze(mean(bath_dists, 'omitnan'))/...
-    max(bath_dists,[], 'all');
-d.bathroom_RDM.ratingRDM(end).name = 'GazeDist';
-d.bathroom_RDM.ratingRDM(end).color = [0, 0, 0]';
+% get RDM for time bins
+tpPerBin = round(cfg.binSize/(1/cfg.ETSamplingFrequency));
+nBins = nTimesteps - tpPerBin;
 
-if ~isempty(gcp('nocreate'))
-    delete(gcp('nocreate'));
-end
+% get mean dist for each bin
+binedDistBath = nan(length(kitImgs), cfg.n, cfg.n, nBins);
+binedDistKit = nan(length(kitImgs), cfg.n, cfg.n, nBins);
+for iBin = 1:nBins
+    binSample = d.bathDistsAll(:, :, :, iBin:iBin+tpPerBin-1);
+    binedDistBath(:, :, :, iBin) = mean(binSample, 4, 'omitnan');
+    binSample = d.kitDistsAll(:, :, :, iBin:iBin+tpPerBin-1);
+    binedDistKit(:, :, :, iBin) = mean(binSample, 4, 'omitnan');
+end 
+
+% mean across images per category
+% kitchen
+if isfield(d, "kitchen_RDM")
+    idx = length({d.kitchen_RDM.ratingRDM.RDM}) + 1;
+else
+    idx = 1;
+end 
+d.kitchen_RDM.ratingRDM(idx).RDM = squeeze(mean(kitDists, 'omitnan'))/...
+    max(kitDists,[], 'all');
+d.kitchen_RDM.ratingRDM(idx).name = 'GazeDist';
+d.kitchen_RDM.ratingRDM(idx).color = [0, 0, 0]';
+
+% bined distances
+d.kitchen_RDM.ratingRDM(idx+1).RDM = squeeze(mean(binedDistKit, 'omitnan'))/...
+    max(binedDistKit,[], 'all');
+d.kitchen_RDM.ratingRDM(idx+1).name = 'binedGazeDist';
+d.kitchen_RDM.ratingRDM(idx+1).color = [0, 0, 0]';
+
+% bathroom
+if isfield(d, "bathroom_RDM")
+    idx = length({d.bathroom_RDM.ratingRDM.RDM}) + 1;
+else
+    idx = 1;
+end 
+d.bathroom_RDM.ratingRDM(idx).RDM = squeeze(mean(bathDists, 'omitnan'))/...
+    max(bathDists,[], 'all');
+d.bathroom_RDM.ratingRDM(idx).name = 'GazeDist';
+d.bathroom_RDM.ratingRDM(idx).color = [0, 0, 0]';
+
+% bined distances
+d.bathroom_RDM.ratingRDM(idx+1).RDM = squeeze(mean(binedDistBath, 'omitnan'))/...
+    max(binedDistKit,[], 'all');
+d.bathroom_RDM.ratingRDM(idx+1).name = 'binedGazeDist';
+d.bathroom_RDM.ratingRDM(idx+1).color = [0, 0, 0]';
+
+
 
 end
