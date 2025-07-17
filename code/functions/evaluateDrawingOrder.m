a9f8319847bf840eac0d591658804bc3b18de001
michaengeser <125463198+michaengeser@users.noreply.github.com>
2025-07-17 10:29:29 +0200

    Add drawings order

---
+++ code/functions/evaluateDrawingOrder.m
@@ -0,0 +1,14 @@
+drawingOrder = readtable(fullfile(pwd, '..', 'DrawingOrderExp1.xlsx'),'Format','auto');
+allObjects = table2cell(drawingOrder(:, 4:end));
+allObjects = reshape(allObjects, [1, numel(allObjects)]);
+
+realObjects = ones(1, numel(allObjects));
+for o = 1:numel(allObjects)
+    if isempty(allObjects{o}) || all(isnan(allObjects{o}))
+        realObjects(o) = 0;
+    end
+end
+
+allObjects = allObjects(logical(realObjects));
+uniqueObjects = unique(allObjects);
+
